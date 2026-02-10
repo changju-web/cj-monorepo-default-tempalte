8b0088443ea4a09f173ea5af60c2d75814201191
wangjiahui <13226651554@163.com>
2026-02-10 11:26:03 +0800

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,22 @@
+// @ts-check
+
+/** @type {import("prettier").Config} */
+export default {
+  /** 换行长度 */
+  printWidth: 100,
+
+  /** tab 宽度 */
+  tabWidth: 2,
+
+  /** 在语句末尾强制分号 */
+  semi: false,
+
+  /** 强制使用单引号 */
+  singleQuote: true,
+
+  /** 结束换行符号类型 */
+  endOfLine: "auto",
+
+  /** 尾随逗号模式 */
+  trailingComma: "none"
+};
